482896377e36bb2e495ac8802ef6d320ac40eb0c
prunell579 <mariocastillo6@hotmail.com>
2024-10-24 21:51:16 +0200

    time series page creation (hardcoded)

--- app.py
+++ app.py
@@ -14,5 +14,10 @@ def index():
     return render_template('index.html', tickers=list(comp.keys()),
                            weights=list(comp.values()))
 
+@app.route('/timeseries')
+def pf_time_series():
+    return render_template('time_series.html')
+
+
 if __name__ == '__main__':
     app.run(debug=True)
