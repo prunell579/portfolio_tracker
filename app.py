1d821af2d6aac2b5dbbeb9a10ead92db5b450e0c
prunell579 <mariocastillo6@hotmail.com>
2024-11-17 19:42:10 +0100

    plug pf data to values time series

--- app.py
+++ app.py
@@ -1,10 +1,26 @@
-from flask import Flask, render_template
+from flask import Flask, json, render_template
 import sys
+import datetime as dt
 
 sys.path.append('.')
 import model.portfoliov2 as pf
 
 app = Flask(__name__)
+
+def generate_dates_vector(starting_date: dt.datetime) -> list:
+    today = dt.datetime.today()
+
+    dates = [starting_date]
+    current_date = starting_date
+
+    while current_date < today:
+        current_date = current_date + dt.timedelta(15)
+        dates.append(current_date)
+
+    dates[-1] = today
+
+    return dates
+
 
 @app.route('/')
 def index():
@@ -16,7 +32,26 @@ def index():
 
 @app.route('/timeseries')
 def pf_time_series():
-    return render_template('time_series.html')
+    my_pf = pf.load_portfolio_db()
+
+    values = []
+    max_attempts = 5
+    dates_iso8601 = []
+    for date in generate_dates_vector(my_pf.get_first_purchase_date()):
+        dates_iso8601.append(dt.datetime.isoformat(date))
+        for attempt in range(1,max_attempts + 1):
+            try:
+                values.append(my_pf.get_portfolio_value_at_date(date))
+                break
+            except ValueError:
+                if attempt == max_attempts:
+                    raise ValueError(f'Problem retrieving portfolio value after {max_attempts} retries')
+                date -= dt.timedelta(-1)
+
+    return render_template('time_series.html',
+                           dates=json.dumps(dates_iso8601),
+                           values=json.dumps(values)
+                           )
 
 
 if __name__ == '__main__':
